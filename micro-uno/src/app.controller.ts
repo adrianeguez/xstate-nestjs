203ce6c921bf6caaf998967d78b0ce139081e078
Adrian Eguez <eadepto@hotmail.com>
2023-07-10 12:04:41 -0500

    cambios

--- micro-uno/src/app.controller.ts
+++ micro-uno/src/app.controller.ts
@@ -1,25 +1,21 @@
-import {BadRequestException, Controller, Get} from '@nestjs/common';
-import {AppService} from './app.service';
-import {
-    MessagePattern,
-    Payload
-} from '@nestjs/microservices';
-import {MicrounoMessagesEnum} from "./enums/microuno-messages.enum";
+import { BadRequestException, Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { MicrounoMessagesEnum } from './enums/microuno-messages.enum';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {
-    }
+  constructor(private readonly appService: AppService) {}
 
-    @Get()
-    getHello(): string {
-        return this.appService.getHello();
-    }
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
 
-    @MessagePattern(MicrounoMessagesEnum.Transaccion)
-    async create(@Payload() data: any) {
-        console.log(data);
-        throw new BadRequestException('EL AMOR');
-        // return {mensaje: 'ok'};
-    }
+  @MessagePattern(MicrounoMessagesEnum.Transaccion)
+  async create(@Payload() data: any) {
+    console.log(data);
+    // throw new BadRequestException('EL AMOR');
+    return { mensaje: 'ok' };
+  }
 }
--- micro-uno/src/main.ts
+++ micro-uno/src/main.ts
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {
@@ -7,17 +7,19 @@ import {
   Transport,
   RmqOptions,
 } from '@nestjs/microservices';
-import {RabbitmqQueuesEnum} from "./enums/rabbitmq-queues.enum";
-import {CONFIG} from "./config/environment";
+import { RabbitmqQueuesEnum } from './enums/rabbitmq-queues.enum';
+import { CONFIG } from './config/environment';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
+  const appMicroservicio = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.RMQ,
     options: {
       urls: [CONFIG.AMQP_URL],
       queue: RabbitmqQueuesEnum.MicroUno,
     },
   });
-  await app.listen();
+  await appMicroservicio.listen();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3001);
 }
 bootstrap();
